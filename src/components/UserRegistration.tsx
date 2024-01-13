6eb361c3fbb3e95b90d55f9fa1b074bd62c3f6ea
Ben-yon <b.ikeh@yahoo.com>
2024-01-13 22:55:44 +0000

    responsive for lg and sm screens @WIP

--- src/components/UserRegistration.tsx
+++ src/components/UserRegistration.tsx
@@ -77,7 +77,7 @@ export const UserRegistration = () => {
           <img
             src={`${media.whiteLogo}`}
             alt="LOGO"
-            className="lg:absolute lg:w-[221px] lg:h-[90px] lg:bottom-[39px] lg:right-[35px] sm:absolute sm:w-[133px] sm:h-[55px] sm:bottom-[32px] sm:right-[31px]"
+            className="lg:absolute lg:w-[221px] lg:h-[90px] lg:bottom-[39px] lg:right-[35px] sm:absolute sm:w-[133px] sm:h-[55px] sm:bottom-[32px] sm:right-[31px] xsm:absolute xsm:w-[133px] xsm:h-[55px] xsm:-bottom-80 xsm:right-[31px]"
           />
         </div>
         <div className="lg:relative lg:flex lg:items-center lg:justify-center lg:p-12">
@@ -91,8 +91,8 @@ export const UserRegistration = () => {
                 ]}
               />
             </div>
-            <div className="lg:ml-36 lg:flex-1 sm:p-10">
-              <strong className="lg:text-5xl text-primary mt-15 sm:text-4xl">
+            <div className="lg:ml-36 lg:flex-1 sm:p-10 xsm:p-8 xsm:items-center ">
+              <strong className="lg:text-5xl text-primary mt-15 sm:text-4xl xsm:text-3xl">
                 Register
               </strong>
               <p className="text-primary mt-2 mb-2 lg:text-xl sm:text-xs">
@@ -100,7 +100,7 @@ export const UserRegistration = () => {
               </p>
               <form onSubmit={handleSubmit}>
                 <div className="absolute mt-[35px] flex flex-col mr-[32px] sm:mt-[30.75px] sm:space-y-0">
-                  <div className="sm:relative sm:bottom-4">
+                  <div className="sm:relative sm:bottom-2 xsm:relative xsm:bottom-2">
                     <input
                       type="file"
                       accept="image/*"
@@ -119,23 +119,23 @@ export const UserRegistration = () => {
                     >
                       <img
                         src={`${media.upload}`}
-                        className={`${styles.imageUploader} absolute `}
+                        className={`${styles.imageUploader} absolute sm:w-[80.52px] sm:h-[80.52px] xsm:w-[80.52px] xsm:h-[80.52px]`}
                       />
                       <img
                         ref={uploadedImageRef}
                         src={imageState.previewSrc}
                         // alt="Upload Image"
-                        className={`${styles.imageUploader} absolute`}
+                        className={`${styles.imageUploader} absolute sm:w-[80.52px] sm:h-[80.52px] xsm:w-[80.52px] xsm:h-[80.52px]`}
                       />
                     </div>
                   </div>
-                  <div className="ml-40 sm:flex sm:flex-col">
+                  <div className="lg:ml-40 sm:flex sm:flex-col sm:ml-28 xsm:flex xsm:flex-col xsm:ml-28">
                     <input
                       type="text"
                       name="firstName"
                       value={formData.firstName}
                       onChange={handleChange}
-                      className="lg:w-[295px] focus:outline-none lg:h-[54px] border lg:rounded-[15px] lg:text-2xl text-white placeholder-gray-300 p-3 sm:h-[37.5px] sm:rounded-[10px] sm:text-xs"
+                      className="lg:w-[295px] focus:outline-none lg:h-[54px] border lg:rounded-[15px] lg:text-2xl text-white placeholder-gray-300 p-3 sm:w-[204.84px] sm:h-[37.5px] sm:rounded-[10px] sm:text-xs xsm:h-[35.5px] xsm:w-[200.84px] xsm:rounded-[8px] xsm:text-xs"
                       placeholder="First Name"
                     />
                     <input
@@ -143,7 +143,7 @@ export const UserRegistration = () => {
                       name="lastName"
                       value={formData.lastName}
                       onChange={handleChange}
-                      className="lg:w-[295px] lg:h-[54px] focus:outline-none border lg:rounded-[15px] mt-[15px] lg:text-2xl text-white placeholder-gray-300 p-3 sm:h-[37.5px] sm:rounded-[10px] sm:text-xs sm:mt-[10.42px]"
+                      className="lg:w-[295px] lg:h-[54px] focus:outline-none border lg:rounded-[15px] mt-[15px] lg:text-2xl text-white placeholder-gray-300 p-3 sm:w-[204.84px] sm:h-[37.5px] sm:rounded-[10px] sm:text-xs sm:mt-[10.42px] xsm:h-[35.5px] xsm:w-[200.84px] xsm:rounded-[8px] xsm:text-xs"
                       placeholder="Last Name"
                     />
                   </div>
@@ -155,7 +155,7 @@ export const UserRegistration = () => {
                       name="date"
                       value={formData.date}
                       onChange={handleChange}
-                      className="lg:w-[181px] lg:h-[54px] focus:outline-none border lg:rounded-[15px] lg:text-xl mt-48 text-white uppercase placeholder-gray-300 p-2 mr-[24px] sm:w-[141.12px] sm:h-[37.23px] sm:rounded-[10.42px] sm:text-xs"
+                      className="lg:w-[181px] lg:h-[54px] focus:outline-none border lg:rounded-[15px] lg:text-xl lg:mt-44 text-white uppercase placeholder-gray-300 p-2 mr-[24px] sm:w-[141.12px] sm:h-[37.23px] sm:rounded-[10.42px] sm:text-xs sm:mt-32 xsm:w-[141.12px] xsm:h-[36.23px] xsm:rounded-[8px] xsm:text-xs xsm:mt-36"
                       placeholder="DD/MM/YYYY"
                     />
                     <input
@@ -163,14 +163,14 @@ export const UserRegistration = () => {
                       name="daysOfMonth"
                       value={formData.daysOfMonth}
                       onChange={handleChange}
-                      className="lg:w-[253px] lg:h-[54px] focus:outline-none border lg:rounded-[15px] lg:text-xl mt-48 text-white placeholder-gray-300 p-3 leading-6 sm:w-[148.05px] sm:h-[37.5px] sm:text-xs sm:rounded-[10.42px]"
+                      className="lg:w-[253px] lg:h-[54px] focus:outline-none border lg:rounded-[15px] lg:text-xl lg:mt-44 text-white placeholder-gray-300 p-3 leading-6 sm:w-[148.05px] sm:h-[37.5px] sm:text-xs sm:rounded-[10.42px] sm:mt-32 xsm:w-[148.05px] xsm:h-[35.5px] xsm:text-xs xsm:rounded-[8px] xsm:mt-36"
                       placeholder="Days per month"
                     />
                   </div>
 
                   <input
                     type="text"
-                    className="lg:w-[716px] focus:outline-none lg:h-[54px] rounded-[15px] mt-[15px] lg:text-xl border text-white placeholder-gray-300 leading-6 p-3 sm:w-[514.27px] sm:h-[37.23px] sm:rounded-[10.42px] sm:text-xs"
+                    className="lg:w-[716px] focus:outline-none lg:h-[54px] rounded-[15px] mt-[15px] lg:text-xl border text-white placeholder-gray-300 leading-6 p-3 sm:w-[514.27px] sm:h-[37.23px] sm:rounded-[10.42px] sm:text-xs xsm:w-[314.27px] xsm:h-[35.23px] xsm:rounded-[8px] xsm:text-xs"
                     placeholder="Address"
                     name="address"
                     value={formData.address}
@@ -178,7 +178,7 @@ export const UserRegistration = () => {
                   />
                   <input
                     type="text"
-                    className="lg:w-[716px] focus:outline-none lg:h-[54px] rounded-[15px] mt-[15px] lg:text-xl border text-white placeholder-gray-300 leading-6 p-3 sm:w-[514.27px] sm:h-[37.23px] sm:rounded-[10.42px] sm:text-xs"
+                    className="lg:w-[716px] focus:outline-none lg:h-[54px] rounded-[15px] mt-[15px] lg:text-xl border text-white placeholder-gray-300 leading-6 p-3 sm:w-[514.27px] sm:h-[37.23px] sm:rounded-[10.42px] sm:text-xs xsm:w-[314.27px] xsm:h-[35.23px] xsm:rounded-[8px] xsm:text-xs"
                     placeholder="Street Address"
                     name="streetAddress"
                     value={formData.streetAddress}
@@ -187,7 +187,7 @@ export const UserRegistration = () => {
                   <div className="flex">
                     <input
                       type="text"
-                      className="lg:w-[282px] lg:h-[54px]  focus:outline-none border rounded-[15px] mt-[15px] mr-[20px] lg:text-xl text-white placeholder-gray-300 p-3 leading-6 sm:w-[243.28px] sm:h-[37.23px] sm:rounded-[10.42px] sm:text-xs"
+                      className="lg:w-[282px] lg:h-[54px]  focus:outline-none border rounded-[15px] mt-[15px] mr-[20px] lg:text-xl text-white placeholder-gray-300 p-3 leading-6 sm:w-[243.28px] sm:h-[37.23px] sm:rounded-[10.42px] sm:text-xs xsm:w-[143.28px] xsm:h-[35.23px] xsm:rounded-[8px] xsm:text-xs"
                       placeholder="City"
                       name="city"
                       value={formData.city}
@@ -195,7 +195,7 @@ export const UserRegistration = () => {
                     />
                     <input
                       type="text"
-                      className="lg:w-[420px] lg:h-[54px] lg:border lg:rounded-[15px] border focus:outline-none mt-[15px] lg:text-xl text-white placeholder-gray-300 p-3 leading-6 sm:h-[37.23px] sm:rounded-[10.42px] sm:text-xs sm:w-[259.73px]"
+                      className="lg:w-[420px] lg:h-[54px] lg:border lg:rounded-[15px] border focus:outline-none mt-[15px] lg:text-xl text-white placeholder-gray-300 p-3 leading-6 sm:h-[37.23px] sm:rounded-[10.42px] sm:text-xs sm:w-[259.73px] xsm:h-[35.23px] xsm:rounded-[8px] xsm:text-xs xsm:w-[157.73px]"
                       placeholder="State/Province/Region"
                       name="province"
                       value={formData.province}
@@ -205,7 +205,7 @@ export const UserRegistration = () => {
                   <div className="flex">
                     <input
                       type="text"
-                      className="lg:w-[282px] lg:h-[54px] border focus:outline-none rounded-[15px] mt-[15px] mr-[20px] lg:text-xl text-white placeholder-gray-300 p-3 leading-6 sm:w-[243.28px] sm:h-[37.23px] sm:rounded-[10.42px] sm:text-xs"
+                      className="lg:w-[282px] lg:h-[54px] border focus:outline-none rounded-[15px] mt-[15px] mr-[20px] lg:text-xl text-white placeholder-gray-300 p-3 leading-6 sm:w-[243.28px] sm:h-[37.23px] sm:rounded-[10.42px] sm:text-xs xsm:w-[143.28px] xsm:h-[35.23px] xsm:rounded-[8px] xsm:text-xs"
                       placeholder="ZIP / Postal Code"
                       name="postalCode"
                       value={formData.postalCode}
@@ -213,7 +213,7 @@ export const UserRegistration = () => {
                     />
                     <input
                       type="text"
-                      className="lg:w-[420px] lg:h-[54px] focus:outline-none border rounded-[15px] mt-[15px] lg:text-xl text-white placeholder-gray-300 p-3 leading- sm:h-[37.23px] sm:rounded-[10.42px] sm:text-xs sm:w-[259.73px]"
+                      className="lg:w-[420px] lg:h-[54px] focus:outline-none border rounded-[15px] mt-[15px] lg:text-xl text-white placeholder-gray-300 p-3 leading- sm:h-[37.23px] sm:rounded-[10.42px] sm:text-xs sm:w-[259.73px] xsm:h-[35.23px] xsm:rounded-[8px] xsm:text-xs xsm:w-[159.73px]"
                       placeholder="Occupation"
                       name="occupation"
                       value={formData.occupation}
@@ -223,7 +223,7 @@ export const UserRegistration = () => {
                   <div className="flex">
                     <input
                       type="text"
-                      className="lg:w-[282px] lg:h-[54px] focus:outline-none border rounded-[15px] mt-[15px] mr-[20px] lg:text-xl text-white placeholder-gray-300 p-3 leading-6 sm:w-[243.28px] sm:h-[37.23px] sm:rounded-[10.42px] sm:text-xs"
+                      className="lg:w-[282px] lg:h-[54px] focus:outline-none border rounded-[15px] mt-[15px] mr-[20px] lg:text-xl text-white placeholder-gray-300 p-3 leading-6 sm:w-[243.28px] sm:h-[37.23px] sm:rounded-[10.42px] sm:text-xs xsm:w-[143.28px] xsm:h-[37.23px] xsm:rounded-[10.42px] xsm:text-xs"
                       placeholder="Skills"
                       name="skills"
                       value={formData.skills}
@@ -231,7 +231,7 @@ export const UserRegistration = () => {
                     />
                     <input
                       type="text"
-                      className="lg:w-[420px] lg:h-[54px] focus:outline-none border rounded-[15px] mt-[15px] lg:text-xl text-white placeholder-gray-300 p-3 leading-6 sm:w-[259.73px] sm:h-[37.23px] sm:rounded-[10.42px] sm:text-xs"
+                      className="lg:w-[420px] lg:h-[54px] focus:outline-none border rounded-[15px] mt-[15px] lg:text-xl text-white placeholder-gray-300 p-3 leading-6 sm:w-[259.73px] sm:h-[37.23px] sm:rounded-[10.42px] sm:text-xs xsm:w-[159.73px] xsm:h-[35.23px] xsm:rounded-[8px] xsm:text-xs"
                       placeholder="Interests"
                       name="interest"
                       value={formData.interest}
@@ -239,7 +239,7 @@ export const UserRegistration = () => {
                     />
                   </div>
                 </div>
-                <button className="bg-primary px-3 py-2 rounded-[15px] border lg:text-xl text-secondary mt-[30px] leading-6 font-bold sm:text-xs sm:mb-7 sm:mt-2 sm:py-[7.55px] sm:px-[17.1px] sm:rounded-[9.4px]">
+                <button className="bg-primary px-3 py-2 rounded-[15px] border lg:text-xl text-secondary mt-[30px] leading-6 font-bold sm:text-xs sm:mb-7 sm:mt-2 sm:py-[7.55px] sm:px-[17.1px] sm:rounded-[9.4px] xsm:text-xs xsm:mb-7 xsm:mt-2 xsm:py-[7.55px] xsm:px-[17.1px] xsm:rounded-[9.4px]">
                   Next
                 </button>
               </form>
