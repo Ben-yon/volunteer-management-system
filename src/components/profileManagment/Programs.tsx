b2510e015feb7e4599b858433ce44f813c28b62f
Benjamin Ikeh <b.ikeh@yahoo.com>
2024-05-07 23:35:22 +0000

    add programs page background

--- src/components/profileManagment/Programs.tsx
+++ src/components/profileManagment/Programs.tsx
@@ -1,7 +1,17 @@
 export const Programs = () => {
-    return (
-        <div>
-            <p>Programs</p>
-        </div>
-    )
-}
+  return (
+    <div className="flex flex-col">
+      <p className="text-[#D9D9D9] text-[15px] font-[700] leading-[18.15px]">
+        MCSS Volunteers
+      </p>
+      <h2 className="text-black font-[700] text-[27px] leading-[32.68px] pb-6">
+        Programs
+      </h2>
+      <div className="w-[1244px] h-[840px] border-[0.5px] rounded-[39px] programs-gradient">
+
+      </div>
+    </div>
+  );
+};
+
+
