4dbf5faaab19d040f3d6676ac3f59853239c3894
Benjamin Ikeh <b.ikeh@yahoo.com>
2024-05-26 00:30:00 +0000

    add file for password reset

---
+++ src/components/profileManagment/ViewProgram.tsx
@@ -0,0 +1,12 @@
+export const ViewProgram: React.FC = () => {
+  return (
+    <div className="flex flex-col justify-center">
+      <p className="text-[#D9D9D9] text-[15px] font-[700] leading-[18.15px]">
+        MCSS Volunteers
+      </p>
+      <h2 className="text-black font-[700] text-[27px] leading-[32.68px] pb-6">
+        Programs
+      </h2>
+    </div>
+  );
+};
