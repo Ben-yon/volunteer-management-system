a7183b59b49de1c95e73f443a61374906bb155dc
Benjamin Ikeh <b.ikeh@yahoo.com>
2024-08-29 00:25:07 +0000

    refactor code for get months

--- src/utils/specificMonth.ts
+++ src/utils/specificMonth.ts
@@ -1,23 +1,18 @@
+const monthNames: string[] = [
+  "January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+];
+
 export function getPreviousMonthName(): string {
     const date = new Date();
-    date.setMonth(date.getMonth() - 1); // Move to the previous month
-  
-    const monthNames: string[] = [
-      "January", "February", "March", "April", "May", "June",
-      "July", "August", "September", "October", "November", "December"
-    ];
+    date.setMonth(date.getMonth() - 1); 
   
     return monthNames[date.getMonth()];
   }
   
 export function getCurrentMonthName(): string {
     const date = new Date();
-    date.setMonth(date.getMonth()); // Move to the previous month
-  
-    const monthNames: string[] = [
-      "January", "February", "March", "April", "May", "June",
-      "July", "August", "September", "October", "November", "December"
-    ];
+    date.setMonth(date.getMonth()); 
   
     return monthNames[date.getMonth()];
   }
